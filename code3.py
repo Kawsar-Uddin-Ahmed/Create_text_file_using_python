a438df9786861a3ba17fa6cea417e214fd670a93
Kawsar Uddin Ahmed <52331860+Kawsar-Uddin-Ahmed@users.noreply.github.com>
2023-07-03 23:05:48 +0600

    Update code3.py

--- code3.py
+++ code3.py
@@ -6,8 +6,8 @@ for a in range(0,i):
 #print(m)
 for d in m :
     with open("name.txt","a") as f:
-         print(d,file=f)
+         print(d,file=f)#Here file=f is used so that the input can be written in the created file. 
     with open("gmail.txt","a") as g:
-         print(d+"@gmail.com",file=g)
+         print(d+"@gmail.com",file=g)#Here file=g is used so that the input can be written in the created file. 
 
     
